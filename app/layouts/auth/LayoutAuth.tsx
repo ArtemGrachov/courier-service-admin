15cf4d38f984c8097b0b4291f7b694ffd0596b46
Artem Grachov <grachov2195@gmail.com>
2025-11-04 09:47:45 +0200

    feat: auth pages layout, CSA-5

--- app/layouts/auth/LayoutAuth.tsx
+++ app/layouts/auth/LayoutAuth.tsx
@@ -1,12 +1,29 @@
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
 import type { ComponentType } from 'react';
 import { Outlet } from 'react-router';
+import Toolbar from '@mui/material/Toolbar';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+
+import LanguageSwitch from '~/components/other/LanguageSwitch';
+import ThemeSwitch from '~/components/other/ThemeSwitch';
 
 const LayoutAuth: ComponentType = () => {
   return (
-    <Stack direction="column" alignItems="center" justifyContent="center" height="100%">
-      <Outlet />
+    <Stack direction="column" alignItems="stretch" height="100%">
+      <Toolbar>
+        <Stack
+          sx={{ marginLeft: 'auto' }}
+          direction="row"
+          spacing={1}
+        >
+          <LanguageSwitch />
+          <ThemeSwitch />
+        </Stack>
+      </Toolbar>
+      <Box margin="auto" display="flex" justifyContent="center" width="100%">
+        <Outlet />
+      </Box>
+      <Toolbar />
     </Stack>
   )
 }
