9cd427bed6125b1fcaaf4383bbb558272124afe3
Artem Grachov <grachov2195@gmail.com>
2025-11-04 22:43:27 +0200

    feat: couriers list refresh and add courier buttons, CSA-7

--- app/routes/ViewCouriers/ViewCouriers.tsx
+++ app/routes/ViewCouriers/ViewCouriers.tsx
@@ -2,11 +2,10 @@ import { useEffect, type ComponentType } from 'react';
 import { Box } from '@mui/material';
 import { observer } from 'mobx-react-lite';
 
-import { EStatus } from '~/constants/status';
-
 import { CouriersProvider, useCouriersCtx } from '~/providers/couriers';
 
 import { useErrorSnackbar } from '~/hooks/other/use-error-snackbar';
+import CouriersHeader from './components/CouriersHeader';
 import CouriersTable from '~/components/couriers/CouriersTable';
 
 const ViewCouriers: ComponentType = observer(() => {
@@ -32,10 +31,12 @@ const ViewCouriers: ComponentType = observer(() => {
   return (
     <Box
       flexDirection="column"
+      display="flex"
       gap={2}
       padding={3}
       width="100%"
     >
+      <CouriersHeader />
       <CouriersTable
         isProcessing={store.isProcessing}
         items={store.data?.data}
---
+++ app/routes/ViewCouriers/components/CouriersHeader.tsx
@@ -0,0 +1,33 @@
+import type { ComponentType } from 'react';
+import { useTranslation } from 'react-i18next';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RefreshIcon from '@mui/icons-material/Refresh';
+
+const CouriersHeader: ComponentType = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Stack direction="row" justifyContent="space-between" gap={4}>
+      <Stack direction="row" gap={2}>
+        <IconButton color="info">
+          <RefreshIcon />
+        </IconButton>
+        <Button
+          variant="contained"
+          color="success"
+          startIcon={<AddCircleOutlineIcon />}
+        >
+          {t('view_couriers.couriers_header.add_courier')}
+        </Button>
+      </Stack>
+      <div>
+        asd
+      </div>
+    </Stack>
+  )
+}
+
+export default CouriersHeader;
