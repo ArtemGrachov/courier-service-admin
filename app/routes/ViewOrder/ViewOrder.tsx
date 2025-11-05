ab1f21d0d484848ac0da96e02874a74ef3e9782f
Artem Grachov <grachov2195@gmail.com>
2025-11-06 00:56:36 +0200

    feat: order page error handler, CSA-13

--- app/routes/ViewOrder/ViewOrder.tsx
+++ app/routes/ViewOrder/ViewOrder.tsx
@@ -1,22 +1,51 @@
-import type { ComponentType } from 'react';
-import { useLoaderData } from 'react-router';
+import { useMemo, type ComponentType } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 import type { Route } from '.react-router/types/app/routes/ViewOrder/+types/ViewOrder';
+import Box from '@mui/material/Box';
+import { observer } from 'mobx-react-lite';
 
 import { EStatus } from '~/constants/status';
 
 import { OrderProvider, useOrderCtx } from '~/providers/order';
 import type { IOrderStoreData } from '~/providers/order/store';
 import { fetchOrder } from '~/providers/order/data';
+import { ReloadPageProvider } from '~/providers/reload-page';
 
-const ViewOrder: ComponentType = () => {
-  const { store: orderStore } = useOrderCtx();
+import PageError from '~/components/other/PageError';
+
+const ViewOrder: ComponentType = observer(() => {
+  const { store: orderStore, fetch } = useOrderCtx();
+  const { orderId } = useParams();
+
+  const showPageError = useMemo(() => {
+    return orderStore.isError || orderStore.getError;
+  }, [orderStore.isError, orderStore.getError]);
+
+  const reloadPageData = () => {
+    fetch(+orderId!)
+  }
 
   return (
-    <div>
-      @todo
-    </div>
+    <Box
+      flexDirection="column"
+      display="flex"
+      gap={2}
+      padding={3}
+      width="100%"
+      boxSizing="border-box"
+    >
+      <ReloadPageProvider reloadFunction={reloadPageData}>
+        {showPageError && (
+          <PageError
+            isProcessing={orderStore.isProcessing}
+            error={orderStore.getError}
+          />
+        )}
+      </ReloadPageProvider>
+      {!showPageError && <div>@todo</div>}
+    </Box>
   )
-}
+})
 
 const Wrapper: ComponentType = () => {
   const loaderData = useLoaderData<Awaited<ReturnType<typeof clientLoader>>>();
