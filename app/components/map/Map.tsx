1b3ed90d0c299db1bd71625211abede1a6b97bc1
Artem Grachov <grachov2195@gmail.com>
2025-11-06 01:47:51 +0200

    feat: map component, CSA-15

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -1,10 +1,44 @@
-import type { ComponentType } from 'react';
+import { useEffect, useRef, type ComponentType } from 'react';
+import { Box } from '@mui/material';
+import L from 'leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 const Map: ComponentType = () => {
+  const mapRef = useRef<HTMLElement | null>(null);
+  const map = useRef<L.Map | null>(null);
+
+  const initMap = () => {
+    if (map.current) {
+      return;
+    }
+
+    map.current = L.map(mapRef.current!).setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map.current);
+  }
+
+  const destroyMap = () => {
+    if (!map.current) {
+      return;
+    }
+
+    map.current.remove();
+    map.current = null;
+  }
+
+  useEffect(() => {
+    initMap();
+
+    return () => destroyMap();
+  }, []);
+
   return (
-    <div>
-      @todo map
-    </div>
+    <Box width="100%" height="100%" ref={mapRef}>
+    </Box>
   )
 }
 
