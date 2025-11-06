ca6a92d89b07626e9304b518d499a4d0c8f05466
Artem Grachov <grachov2195@gmail.com>
2025-11-06 11:12:41 +0200

    feat: render markers, CSA-15

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import L from 'leaflet';
 
 import type { IOrder } from '~/types/models/order';
+import type { IGeoPos } from '~/types/models/geo-pos';
 
 import 'leaflet/dist/leaflet.css';
 
@@ -10,9 +11,76 @@ interface IProps {
   orders?: IOrder[];
 }
 
+interface IMarkerData {
+  key: string;
+  location: IGeoPos;
+}
+
+interface IMarker {
+  lMarker: L.Marker;
+  key: string | number;
+}
+
 const Map: ComponentType<IProps> = ({ orders }) => {
   const mapRef = useRef<HTMLElement | null>(null);
   const map = useRef<L.Map | null>(null);
+  const markers = useRef<Record<string | number, IMarker>>({});
+
+  useEffect(() => {
+    renderMarkers();
+  }, [orders]);
+
+  const renderMarkers = () => {
+    if (!map.current) {
+      return;
+    }
+
+    const mrkrs = markers.current;
+
+    const markersData = orders?.reduce((acc, curr) => {
+      const { senderGeoPos, receiverGeoPos, courier } = curr;
+
+      if (senderGeoPos) {
+        acc.push({
+          key: `sender_${curr.id}`,
+          location: senderGeoPos,
+        });
+      }
+
+      if (receiverGeoPos) {
+        acc.push({
+          key: `receiver_${curr.id}`,
+          location: receiverGeoPos,
+        });
+      }
+
+      if (courier?.location) {
+        acc.push({
+          key: `courier_${courier.id}`,
+          location: courier.location,
+        });
+      }
+
+      return acc;
+    }, [] as IMarkerData[]);
+
+    markersData?.forEach(markerData => {
+      let marker = mrkrs[markerData.key];
+
+      if (mrkrs[markerData.key]) {
+        marker.lMarker.setLatLng([markerData.location.lat, markerData.location.lng]);
+        return;
+      }
+
+      marker = {
+        key: markerData.key,
+        lMarker: L.marker([markerData.location.lat, markerData.location.lng]).addTo(map.current!)
+      };
+
+
+      mrkrs[markerData.key] = marker;
+    });
+  }
 
   const initMap = () => {
     if (map.current) {
@@ -26,29 +94,7 @@ const Map: ComponentType<IProps> = ({ orders }) => {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map.current);
 
-    const senderMarkers = orders?.map(order => {
-      return L.marker([order.senderGeoPos.lat, order.senderGeoPos.lng]);
-    });
-
-    const receiverMarkers = orders?.map(order => {
-      return L.marker([order.receiverGeoPos.lat, order.receiverGeoPos.lng]);
-    });
-
-    const courierMarkers = orders?.filter(o => !!o.courier).map(order => {
-      return L.marker([order.courier!.location.lat, order.courier!.location.lng]);
-    });
-
-    senderMarkers?.forEach(marker => {
-      marker.addTo(map.current!);
-    });
-
-    receiverMarkers?.forEach(marker => {
-      marker.addTo(map.current!);
-    });
-
-    courierMarkers?.forEach(marker => {
-      marker.addTo(map.current!);
-    });
+    renderMarkers();
   }
 
   const destroyMap = () => {
@@ -58,6 +104,7 @@ const Map: ComponentType<IProps> = ({ orders }) => {
 
     map.current.remove();
     map.current = null;
+    markers.current = {};
   }
 
   useEffect(() => {
