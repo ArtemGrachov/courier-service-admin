73dadd9e03de814e2d6060d5ca7e4b8958f8d17a
Artem Grachov <grachov2195@gmail.com>
2025-11-06 23:38:16 +0200

    feat: courier page details section, CSA-43

--- app/components/couriers/CourierDetails.tsx
+++ app/components/couriers/CourierDetails.tsx
@@ -1,6 +1,6 @@
 import { type ComponentType } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from '@mui/material';
+import { Grid, Link } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -22,61 +22,79 @@ const CourierDetails: ComponentType<IProps> = ({ courier }) => {
   return (
     <Card>
       <CardContent>
-        <Stack direction="row" gap={2} justifyContent="space-between">
-          <Box gap={1}>
-            <Typography variant="subtitle1" component="div">
-              {t('courier_details.id')} #{courier.id}
-            </Typography>
-            <Typography variant="h5" component="div">
-              {courier.name}
-            </Typography>
-          </Box>
-          <Box gap={1}>
-            <Typography variant="subtitle1" component="div">
-              {t('courier_details.rating')}
-            </Typography>
-            <Rating rating={courier.rating} />
-          </Box>
-          <Box gap={1}>
-            <Typography variant="subtitle1" component="div" color="warning">
-              {t('courier_details.current_orders_count')}
-            </Typography>
-            <Typography variant="h5" component="div" color="warning">
-              {courier.currentOrdersCount}
-            </Typography>
-          </Box>
-          <Box gap={1}>
-            <Typography variant="subtitle1" component="div">
-              {t('courier_details.total_orders_count')}
-            </Typography>
-            <Typography variant="h5" component="div">
-              {courier.totalOrdersCount}
-            </Typography>
-          </Box>
-          <Box gap={1}>
-            <Typography variant="subtitle1" component="div">
-              {t('courier_details.phone')}
-            </Typography>
-            {phoneNumber ? (<Link
-              component="a"
-              href={`tel:${phoneNumber}`}
-              target="_blank"
-            >
-              {phoneNumber}
-            </Link>) : '-'}
-          </Box>
-          <Box gap={1}>
-            <Typography variant="subtitle1" component="div">
-              {t('courier_details.email')}
-            </Typography>
-            {email ? (<Link
-              component="a"
-              href={`mailto:${email}`}
-              target="_blank"
-            >
-              {email}
-            </Link>) : '-'}
-          </Box>
+        <Stack gap={2}>
+          <Grid container spacing={2}>
+            <Grid size={6} gap={2}>
+              <Box gap={1}>
+                <Typography variant="subtitle1" component="div">
+                  {t('courier_details.id')} #{courier.id}
+                </Typography>
+                <Typography variant="h5" component="div">
+                  {courier.name}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid size={6} gap={2}>
+              <Box gap={1}>
+                <Typography variant="subtitle1" component="div">
+                  {t('courier_details.rating')}
+                </Typography>
+                <Rating rating={courier.rating} />
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid size={6} gap={2}>
+              <Box gap={1}>
+                <Typography variant="subtitle1" component="div">
+                  {t('courier_details.phone')}
+                </Typography>
+                {phoneNumber ? (<Link
+                  component="a"
+                  href={`tel:${phoneNumber}`}
+                  target="_blank"
+                >
+                  {phoneNumber}
+                </Link>) : '-'}
+              </Box>
+            </Grid>
+            <Grid size={6} gap={2}>
+              <Box gap={1}>
+                <Typography variant="subtitle1" component="div">
+                  {t('courier_details.email')}
+                </Typography>
+                {email ? (<Link
+                  component="a"
+                  href={`mailto:${email}`}
+                  target="_blank"
+                >
+                  {email}
+                </Link>) : '-'}
+              </Box>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2}>
+            <Grid size={6} gap={2}>
+              <Box gap={1}>
+                <Typography variant="subtitle1" component="div" color="warning">
+                  {t('courier_details.current_orders_count')}
+                </Typography>
+                <Typography variant="h5" component="div" color="warning">
+                  {courier.currentOrdersCount}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid size={6} gap={2}>
+              <Box gap={1}>
+                <Typography variant="subtitle1" component="div">
+                  {t('courier_details.total_orders_count')}
+                </Typography>
+                <Typography variant="h5" component="div">
+                  {courier.totalOrdersCount}
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
         </Stack>
       </CardContent>
     </Card>
