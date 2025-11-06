9cbc3bc75655ebac77413eb0b60ec9bf68727edd
Artem Grachov <grachov2195@gmail.com>
2025-11-06 12:38:39 +0200

    feat: marker popups (wip), CSA-15

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -1,15 +1,20 @@
-import { useEffect, useRef, type ComponentType } from 'react';
+import { useEffect, useRef, useState, type ComponentType } from 'react';
 import Box from '@mui/material/Box';
+import Portal from '@mui/material/Portal';
 import L from 'leaflet';
 
 import type { IOrder } from '~/types/models/order';
 import type { IGeoPos } from '~/types/models/geo-pos';
+import type { IClient } from '~/types/models/client';
+import type { ICourier } from '~/types/models/courier';
 
 import 'leaflet/dist/leaflet.css';
 
 import srcIconSender from '~/assets/icons/map/sender.svg';
 import srcIconReceiver from '~/assets/icons/map/receiver.svg';
 import srcIconCourier from '~/assets/icons/map/courier.svg';
+import CourierCard from '~/components/couriers/CourierCard';
+import ClientCard from '~/components/clients/ClientCard';
 
 interface IProps {
   orders?: IOrder[];
@@ -21,15 +26,27 @@ const enum EMarkerTypes {
   COURIER,
 }
 
-interface IMarkerData {
+interface IMarkerData<T> {
   key: string;
   location: IGeoPos;
   type: EMarkerTypes;
+  data: T;
 }
+
+interface IMarkerDataSender extends IMarkerData<IClient> { }
+
+interface IMarkerDataReceiver extends IMarkerData<IClient> { }
+
+interface IMarkerDataCourier extends IMarkerData<ICourier> { }
+
+type MarkerData = IMarkerDataSender | IMarkerDataReceiver | IMarkerDataCourier;
+
+type MarkerKey = string;
 
 interface IMarker {
   lMarker: L.Marker;
-  key: string | number;
+  key: MarkerKey;
+  data: MarkerData;
 }
 
 const ICONS = {
@@ -50,7 +67,8 @@ const ICONS = {
 const Map: ComponentType<IProps> = ({ orders }) => {
   const mapRef = useRef<HTMLElement | null>(null);
   const map = useRef<L.Map | null>(null);
-  const markers = useRef<Record<string | number, IMarker>>({});
+  const markers = useRef<Record<MarkerKey, IMarker>>({});
+  const [markerPopups, setMarkerPopups] = useState<Array<string>>([]);
 
   useEffect(() => {
     renderMarkers();
@@ -66,19 +84,21 @@ const Map: ComponentType<IProps> = ({ orders }) => {
     const markersData = orders?.reduce((acc, curr) => {
       const { senderGeoPos, receiverGeoPos, courier } = curr;
 
-      if (senderGeoPos) {
+      if (senderGeoPos && curr.sender) {
         acc.push({
           key: `sender_${curr.id}`,
           location: senderGeoPos,
           type: EMarkerTypes.SENDER,
+          data: curr.sender,
         });
       }
 
-      if (receiverGeoPos) {
+      if (receiverGeoPos && curr.receiver) {
         acc.push({
           key: `receiver_${curr.id}`,
           location: receiverGeoPos,
           type: EMarkerTypes.RECEIVER,
+          data: curr.receiver,
         });
       }
 
@@ -87,11 +107,12 @@ const Map: ComponentType<IProps> = ({ orders }) => {
           key: `courier_${courier.id}`,
           location: courier.location,
           type: EMarkerTypes.COURIER,
+          data: courier,
         });
       }
 
       return acc;
-    }, [] as IMarkerData[]);
+    }, [] as MarkerData[]);
 
     markersData?.forEach(markerData => {
       let marker = mrkrs[markerData.key];
@@ -118,14 +139,21 @@ const Map: ComponentType<IProps> = ({ orders }) => {
         }
       }
 
+      const lMarker = L.marker(
+        [markerData.location.lat, markerData.location.lng],
+        { icon },
+      )
+        .addTo(map.current!)
+        .addEventListener('click', () => {
+          setMarkerPopups(v => {
+            return Array.from(new Set([...v, markerData.key]));
+          });
+        });
+
       marker = {
         key: markerData.key,
-        lMarker: L.marker(
-          [markerData.location.lat, markerData.location.lng],
-          {
-            icon,
-          },
-        ).addTo(map.current!)
+        data: markerData,
+        lMarker,
       };
 
 
@@ -165,7 +193,36 @@ const Map: ComponentType<IProps> = ({ orders }) => {
   }, []);
 
   return (
-    <Box width="100%" height="100%" ref={mapRef} />
+    <>
+      <div style={{ position: 'absolute', zIndex: 1000 }}>
+        {markerPopups.map(markerKey => {
+          const markerData = markers.current[markerKey];
+
+          if (!markerData) {
+            return null;
+          }
+
+          switch (markerData.data.type) {
+            case EMarkerTypes.SENDER: {
+              return <ClientCard client={markerData.data.data as IClient} isSender={true} />
+            }
+            case EMarkerTypes.RECEIVER: {
+              return <ClientCard client={markerData.data.data as IClient} isReceiver={true} />
+            }
+            case EMarkerTypes.COURIER: {
+              return <CourierCard courier={markerData.data.data as ICourier} />
+            }
+          }
+        })}
+      </div>
+      {/* <div id="portal-test" style={{ background: 'red'}}></div> */}
+      <Box width="100%" height="100%" ref={mapRef} />
+      {/* <Portal container={() => document.getElementById('portal-test')}>
+        <div>
+          test
+        </div>
+      </Portal> */}
+    </>
   )
 }
 
