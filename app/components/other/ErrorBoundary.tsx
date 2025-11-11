c3e6f4d35311111b6f75ece62c8f801a40d7d594
Artem Grachov <grachov2195@gmail.com>
2025-11-11 20:53:08 +0200

    feat: error boundary title, CSA-60

--- app/components/other/ErrorBoundary.tsx
+++ app/components/other/ErrorBoundary.tsx
@@ -1,25 +1,36 @@
 import { useEffect, type ComponentType } from 'react';
 import { useRouteError } from 'react-router';
 import Box from '@mui/material/Box';
+import Portal from '@mui/material/Portal';
+import { useTranslation } from 'react-i18next';
+
+import { useTitlePortalCtx } from '~/providers/title-portal';
 
 import PageError from '~/components/other/PageError';
 
 const ErrorBoundary: ComponentType = () => {
   const error = useRouteError();
+  const titlePortalRef = useTitlePortalCtx();
+  const { t } = useTranslation();
 
   useEffect(() => {
     console.error(error);
   }, []);
 
   return (
-    <Box
-      padding={3}
-      width="100%"
-      height="100%"
-      boxSizing="border-box"
-    >
-      <PageError error={error} />
-    </Box>
+    <>
+      <Portal container={() => titlePortalRef?.current ?? null}>
+        {t('common_errors.page_title')}
+      </Portal>
+      <Box
+        padding={3}
+        width="100%"
+        height="100%"
+        boxSizing="border-box"
+      >
+        <PageError error={error} />
+      </Box>
+    </>
   )
 }
 
