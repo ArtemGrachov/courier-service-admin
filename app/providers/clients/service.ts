55748c45f226dc77474d878d203f13a7adaa6bbd
Artem Grachov <grachov2195@gmail.com>
2025-11-11 12:12:37 +0200

    refactor: removed fetch functions from data providers, CSA-59

--- app/providers/clients/service.ts
+++ app/providers/clients/service.ts
@@ -4,24 +4,11 @@ import { ClientsStore, type IClientsStoreData } from '~/store/clients.store';
 
 import type { IGetClientsQuery } from '~/types/api/clients';
 
-import { fetchClients } from '../../data/fetch-clients';
-
 export const useClientsService = (initialData?: IClientsStoreData) => {
   const clientsStore = useRef<ClientsStore>(null as unknown as ClientsStore);
 
   if (!clientsStore.current) {
     clientsStore.current = new ClientsStore(initialData);
-  }
-
-  const fetch = async (query?: IGetClientsQuery) => {
-    try {
-      clientsStore.current.doGetInit();
-      const data = await fetchClients(query)
-      clientsStore.current.doGetSuccess(data);
-    } catch (err) {
-      clientsStore.current.doGetError(err);
-      throw err;
-    }
   }
 
   const setProcessing = () => {
@@ -30,7 +17,6 @@ export const useClientsService = (initialData?: IClientsStoreData) => {
 
   return {
     store: clientsStore.current,
-    fetch,
     setProcessing,
   };
 }
--- app/providers/courier/service.ts
+++ app/providers/courier/service.ts
@@ -2,26 +2,11 @@ import { useRef } from 'react';
 
 import { CourierStore, type ICourierStoreData } from './store';
 
-import { fetchCourier } from '~/providers/courier/data';
-
 export const useCourierService = (initialData?: ICourierStoreData) => {
   const courierStore = useRef<CourierStore>(null as unknown as CourierStore);
 
   if (!courierStore.current) {
     courierStore.current = new CourierStore(initialData);
-  }
-
-  const fetch = async (courierId: number) => {
-    try {
-      courierStore.current.doGetInit();
-
-      const data = await fetchCourier(courierId)
-
-      courierStore.current.doGetSuccess(data);
-    } catch (err) {
-      courierStore.current.doGetError(err);
-      throw err;
-    }
   }
 
   const setProcessing = () => {
@@ -30,7 +15,6 @@ export const useCourierService = (initialData?: ICourierStoreData) => {
 
   return {
     store: courierStore.current,
-    fetch,
     setProcessing,
   };
 }
