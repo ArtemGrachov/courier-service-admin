267bde66dc455824f129263cbc854c2614ba4457
Artem Grachov <grachov2195@gmail.com>
2025-11-15 13:33:50 +0200

    fix: page reloader search parameters, CSA-61

--- app/providers/reload-page/hooks/use-default-reload.ts
+++ app/providers/reload-page/hooks/use-default-reload.ts
@@ -1,13 +1,16 @@
-import { useNavigate } from 'react-router';
+import { useNavigate, useSearchParams } from 'react-router';
 
 import { useRouteKeyCtx } from '~/providers/route-key';
 
 export const useDefaultReload = () => {
   const routeKeyCtx = useRouteKeyCtx();
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
   return async () => {
-    await navigate('.', { replace: true });
+    await navigate({
+      search: searchParams.toString(),
+    }, { replace: true });
 
     if (routeKeyCtx) {
       routeKeyCtx.updateRouteKey();
