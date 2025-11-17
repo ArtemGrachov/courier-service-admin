1aa3051d03de1b0280e932fa18db7051a6ba5875
Artem Grachov <grachov2195@gmail.com>
2025-11-18 01:08:08 +0200

    fix: upsert courier page

--- app/routes/ViewUpsertCourier/ViewUpsertCourier.tsx
+++ app/routes/ViewUpsertCourier/ViewUpsertCourier.tsx
@@ -92,10 +92,12 @@ const ViewUpsertCourier: ComponentType = observer(() => {
     await navigate(routePath(ROUTES.COURIER, { courierId: courierId ?? 1 }));
   }
 
+  const title = isEdit ? t('view_upsert_courier.title_edit', { id: courier?.id, name: courier?.name }) : t('view_upsert_courier.title_create');
+
   return (
     <ReloadPageProvider reloadFunction={reloadPageData} isDefaultReload={false}>
       <Portal container={() => titlePortalRef?.current ?? null}>
-        {t('view_upsert_courier.title', { id: courier?.id, name: courier?.name })}
+        {title}
       </Portal>
       <Box padding={3}>
         <Box maxWidth={500} margin="auto">
@@ -167,9 +169,10 @@ export { ErrorBoundary };
 export function meta({ loaderData }: Route.MetaArgs) {
   const { t } = i18n;
   const courier = loaderData?.courierState?.data;
+  const title = courier ? t('view_upsert_courier.title_edit', { id: courier?.id, name: courier?.name }) : t('view_upsert_courier.title_create');
 
   return [
-    { title: t('common_meta.title_template', { title: t('view_upsert_courier.title', { id: courier?.id, name: courier?.name }) }) },
+    { title: t('common_meta.title_template', { title }) },
   ];
 }
 
